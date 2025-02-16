4434b0681689eb3ac4562e597409d91c7df25eaf
KDDilshan <kdprojects847@gmail.com>
2025-02-16 11:48:38 +0530

    (modified):user name and roles for respose for user

--- src/main/java/com/kavindu/full_auth/dto/Response/AuthResponse.java
+++ src/main/java/com/kavindu/full_auth/dto/Response/AuthResponse.java
@@ -1,9 +1,15 @@
 package com.kavindu.full_auth.dto.Response;
 
+import com.kavindu.full_auth.entities.Roles;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -11,6 +17,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AuthResponse {
     private String token;
-    private Long expiresIn;
+    private String username;
+    private List<String> roles;
 
+    public AuthResponse(String token, String username, Set<Roles> roles) {
+        this.token = token;
+        this.username = username;
+        this.roles = roles.stream().map(Roles::getName).collect(Collectors.toList());
+    }
 }
